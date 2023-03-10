41ad03cac59140bed8f54fe88a9df2f1c5d1902e
Alexa <alexa.prokofieva@yandex.ru>
2023-03-10 19:26:13 +0300

    Added Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые чила в промежутке от N до  -N
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+while(count <= number)
+{
+  Console.Write($"{count} ");
+  count++;
+}
+
+
