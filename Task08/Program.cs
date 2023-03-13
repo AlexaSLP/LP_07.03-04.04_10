fef98f6a6dee34aa43ade421adc0d15bca446939
Alexa <alexa.prokofieva@yandex.ru>
2023-03-13 22:54:10 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите положительное число больше 0: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (i <= number - 2)
+{
+  Console.Write($"{i}, ");
+  i += 2;
+}
+if (number % 2 == 0)
+  Console.Write(number);
+else
+  Console.Write(number - 1);
