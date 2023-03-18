111f27aa6a6b647ca7420350894e70b3c98cc0ee
Alexa <alexa.prokofieva@yandex.ru>
2023-03-18 23:21:40 +0300

    Added Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+  if (dayNumber >=6 && dayNumber <=8) 
+  {
+    Console.WriteLine("Это выходной день");
+  }
+  else if (dayNumber >= 1 && dayNumber < 7) 
+  {
+    Console.WriteLine("Это не выходной день, а рабочий");
+  }
+ else Console.WriteLine("Это число не соответствует дню недели");
