ec4808abaee65a020659fa411a4e0eedc84c1528
Alexa <alexa.prokofieva@yandex.ru>
2023-03-29 17:19:21 +0300

    Added Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число
+// N выдает произведение чиссел от 1 до N
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ptoductNumbers = ProductNumbers(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {ProductNumbers(number)}");
+
+int ProductNumbers(int num)
+{
+  int prod = 1;
+  for (int i = 2; i <= num; i++)
+  {
+    checked // проверка 
+    {
+      prod = prod * i;
+    }
+  }
+  return prod;
+}
+
